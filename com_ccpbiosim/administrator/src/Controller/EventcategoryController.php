b2c6b79fc0cbed83f460ac5508a01592b3b58fd5
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-02-06 12:16:07 +0000

    add event and category controllers

---
+++ com_ccpbiosim/administrator/src/Controller/EventcategoryController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package    com_ccpbiosim
+ * @copyright  2025 CCPBioSim Team
+ * @license    MIT
+ */
+
+namespace Ccpbiosim\Component\Ccpbiosim\Administrator\Controller;
+
+\defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\FormController;
+
+/**
+ * Eventcategory controller class.
+ */
+class EventcategoryController extends FormController
+{
+	protected $view_list = 'eventcategories';
+}
