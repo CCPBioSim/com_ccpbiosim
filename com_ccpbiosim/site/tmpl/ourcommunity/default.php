a0341ff5108eb35df4abd119341246a4af0ff3d7
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-02-11 12:43:45 +0000

    add site template

---
+++ com_ccpbiosim/site/tmpl/ourcommunity/default.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package    com_ccpbiosim
+ * @copyright  2025 CCPBioSim Team
+ * @license    MIT
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+use \Joomla\CMS\HTML\HTMLHelper;
+use \Joomla\CMS\Factory;
+use \Joomla\CMS\Uri\Uri;
+use \Joomla\CMS\Router\Route;
+use \Joomla\CMS\Language\Text;
+use \Joomla\CMS\Session\Session;
+use Joomla\Utilities\ArrayHelper;
+
+$params  = $this->item->params;
+// Import CSS & JS
+$wa = $this->document->getWebAssetManager();
+$wa->useStyle('com_ccpbiosim.site')
+   ->useScript('com_ccpbiosim.site');
+?>
+
+<?php if ($this->params->get('show_page_heading')) : ?>
+    <div class="page-header">
+        <h2> <?php echo $this->escape($this->params->get('page_heading')); ?> </h2>
+    </div>
+<?php endif; ?>
+<p>code will go here</p>
+
