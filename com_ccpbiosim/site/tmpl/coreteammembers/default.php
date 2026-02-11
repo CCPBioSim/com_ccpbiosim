60324660b3d3262e31fda9dfb767d6de913401c8
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-02-11 15:48:43 +0000

    copy layout code to default layouts

--- com_ccpbiosim/site/tmpl/coreteammembers/default.php
+++ com_ccpbiosim/site/tmpl/coreteammembers/default.php
@@ -34,196 +34,32 @@ $wa = $this->document->getWebAssetManager();
 $wa->useStyle('com_ccpbiosim.site')
    ->useScript('com_ccpbiosim.site');
 ?>
-
 <?php if ($this->params->get('show_page_heading')) : ?>
     <div class="page-header">
         <h1> <?php echo $this->escape($this->params->get('page_heading')); ?> </h1>
     </div>
 <?php endif;?>
-<form action="<?php echo htmlspecialchars(Uri::getInstance()->toString()); ?>" method="post"
-	  name="adminForm" id="adminForm">
-	
-	<div class="table-responsive">
-		<table class="table table-striped" id="coreteammemberList">
-			<thead>
-			<tr>
-				
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_ID', 'a.id', $listDirn, $listOrder); ?>
-					</th>
+<p>Our core team is made up of our chair, administrative support from the chairs institution and CoSeC support staff from UKRI - STFC.</br></p>
 
-					<th >
-						<?php echo HTMLHelper::_('grid.sort', 'JPUBLISHED', 'a.state', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_TITLE', 'a.title', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_FIRSTNAME', 'a.firstname', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_SURNAME', 'a.surname', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_PROFILEPHOTO', 'a.profilephoto', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_ROLE', 'a.role', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_INSITUTION', 'a.insitution', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_GROUPWEBSITE', 'a.groupwebsite', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_PROGRAMME', 'a.programme', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_SOCIAL', 'a.social', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_CHAIR', 'a.chair', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_COSECPROJECTLEAD', 'a.cosecprojectlead', $listDirn, $listOrder); ?>
-					</th>
-
-					<th class=''>
-						<?php echo HTMLHelper::_('grid.sort',  'COM_CCPBIOSIM_CORETEAMMEMBERS_ADMINASSISTANT', 'a.adminassistant', $listDirn, $listOrder); ?>
-					</th>
-
-						<?php if ($canEdit || $canDelete): ?>
-					<th class="center">
-						<?php echo Text::_('COM_CCPBIOSIM_CORETEAMMEMBERS_ACTIONS'); ?>
-					</th>
-					<?php endif; ?>
-
-			</tr>
-			</thead>
-			<tfoot>
-			<tr>
-				<td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
-					<div class="pagination">
-						<?php echo $this->pagination->getPagesLinks(); ?>
-					</div>
-				</td>
-			</tr>
-			</tfoot>
-			<tbody>
-			<?php foreach ($this->items as $i => $item) : ?>
-				<?php $canEdit = $user->authorise('core.edit', 'com_ccpbiosim'); ?>
-				<?php if (!$canEdit && $user->authorise('core.edit.own', 'com_ccpbiosim')): ?>
-				<?php $canEdit = Factory::getApplication()->getIdentity()->id == $item->created_by; ?>
-				<?php endif; ?>
-
-				<tr class="row<?php echo $i % 2; ?>">
-					
-					<td>
-						<?php echo $item->id; ?>
-					</td>
-					<td>
-						<?php $class = ($canChange) ? 'active' : 'disabled'; ?>
-						<a class="btn btn-micro <?php echo $class; ?>" href="<?php echo ($canChange) ? Route::_('index.php?option=com_ccpbiosim&task=coreteammember.publish&id=' . $item->id . '&state=' . (($item->state + 1) % 2), false, 2) : '#'; ?>">
-						<?php if ($item->state == 1): ?>
-							<i class="icon-publish"></i>
-						<?php else: ?>
-							<i class="icon-unpublish"></i>
-						<?php endif; ?>
-						</a>
-					</td>
-					<td>
-						<?php echo $item->title; ?>
-					</td>
-					<td>
-						<?php echo $item->firstname; ?>
-					</td>
-					<td>
-						<?php echo $item->surname; ?>
-					</td>
-					<td>
-												<img src="<?php echo Uri::root() . $item->profilephoto; ?>" alt="Preview" style="max-height: 50px;" />
-					</td>
-					<td>
-						<?php echo $item->role; ?>
-					</td>
-					<td>
-						<?php echo $item->insitution; ?>
-					</td>
-					<td>
-						<?php echo $item->groupwebsite; ?>
-					</td>
-					<td>
-						<?php echo $item->programme; ?>
-					</td>
-					<td>
-						<?php echo $item->social; ?>
-					</td>
-					<td>
-						<?php echo $item->chair; ?>
-					</td>
-					<td>
-						<?php echo $item->cosecprojectlead; ?>
-					</td>
-					<td>
-						<?php echo $item->adminassistant; ?>
-					</td>
-					<?php if ($canEdit || $canDelete): ?>
-						<td class="center">
-							<?php $canCheckin = Factory::getApplication()->getIdentity()->authorise('core.manage', 'com_ccpbiosim.' . $item->id) || $item->checked_out == Factory::getApplication()->getIdentity()->id; ?>
-
-							<?php if($canEdit && $item->checked_out == 0): ?>
-								<a href="<?php echo Route::_('index.php?option=com_ccpbiosim&task=coreteammember.edit&id=' . $item->id, false, 2); ?>" class="btn btn-mini" type="button"><i class="icon-edit" ></i></a>
-							<?php endif; ?>
-							<?php if ($canDelete): ?>
-								<a href="<?php echo Route::_('index.php?option=com_ccpbiosim&task=coreteammemberform.remove&id=' . $item->id, false, 2); ?>" class="btn btn-mini delete-button" type="button"><i class="icon-trash" ></i></a>
-							<?php endif; ?>
-						</td>
-					<?php endif; ?>
-
-				</tr>
-			<?php endforeach; ?>
-			</tbody>
-		</table>
-	</div>
-	<?php if ($canCreate) : ?>
-		<a href="<?php echo Route::_('index.php?option=com_ccpbiosim&task=coreteammemberform.edit&id=0', false, 0); ?>"
-		   class="btn btn-success btn-small"><i
-				class="icon-plus"></i>
-			<?php echo Text::_('COM_CCPBIOSIM_ADD_ITEM'); ?></a>
-	<?php endif; ?>
-
-	<input type="hidden" name="task" value=""/>
-	<input type="hidden" name="boxchecked" value="0"/>
-	<input type="hidden" name="filter_order" value=""/>
-	<input type="hidden" name="filter_order_Dir" value=""/>
-	<?php echo HTMLHelper::_('form.token'); ?>
-</form>
-
-<?php
-	if($canDelete) {
-		$wa->addInlineScript("
-			jQuery(document).ready(function () {
-				jQuery('.delete-button').click(deleteItem);
-			});
-
-			function deleteItem() {
-
-				if (!confirm(\"" . Text::_('COM_CCPBIOSIM_DELETE_MESSAGE') . "\")) {
-					return false;
-				}
-			}
-		", [], [], ["jquery"]);
-	}
-?>
+<div class="container container-md">
+  <div class="row g-4">
+    <?php foreach ($this->items as $i => $item) : ?>
+      <?php $canEdit = $user->authorise('core.edit', 'com_ccpbiosim'); ?>
+      <?php if (!$canEdit && $user->authorise('core.edit.own', 'com_ccpbiosim')): ?>
+        <?php $canEdit = Factory::getApplication()->getIdentity()->id == $item->created_by; ?>
+      <?php endif; ?>
+      <div class="col-12 col-md-6 fade-up">
+        <div class="core-team-card core-team-horizontal">
+          <div class="core-team-image-wrap">
+            <img src="<?php echo Uri::root() . $item->profilephoto; ?>" alt="<?php echo $item->title; ?> <?php echo $item->firstname; ?> <?php echo $item->surname; ?>">
+          </div>
+          <div class="core-team-content">
+            <div class="core-team-name"><?php echo $item->title; ?> <?php echo $item->firstname; ?> <?php echo $item->surname; ?></div>
+            <div class="core-team-institution"><?php echo $item->insitution; ?></div>
+            <p class="core-team-bio"><?php echo $item->role; ?></p>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
+  </div>
+</div>
