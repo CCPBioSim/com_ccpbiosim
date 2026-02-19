bb0df3c3736eaa28ded51d1c3152aeec0e9906bd
James Gebbie-Rayet <james.gebbie@stfc.ac.uk>
2026-02-19 15:54:14 +0000

    fix event display order

--- com_ccpbiosim/site/src/Model/EventsModel.php
+++ com_ccpbiosim/site/src/Model/EventsModel.php
@@ -70,7 +70,7 @@ class EventsModel extends ListModel
 	protected function populateState($ordering = null, $direction = null)
 	{
 		// List state information.
-		parent::populateState("a.id", "ASC");
+		parent::populateState("a.startdatetime", "DESC");
 
 		$app = Factory::getApplication();
 		$list = $app->getUserState($this->context . '.list');
@@ -83,8 +83,8 @@ class EventsModel extends ListModel
 		$value = $app->input->get('limitstart', 0, 'uint');
 		$this->setState('list.start', $value);
 
-		$ordering  = $this->getUserStateFromRequest($this->context .'.filter_order', 'filter_order', "a.id");
-		$direction = strtoupper($this->getUserStateFromRequest($this->context .'.filter_order_Dir', 'filter_order_Dir', "ASC"));
+		$ordering  = $this->getUserStateFromRequest($this->context .'.filter_order', 'filter_order', "a.startdatetime");
+		$direction = strtoupper($this->getUserStateFromRequest($this->context .'.filter_order_Dir', 'filter_order_Dir', "DESC"));
 		
 		if(!empty($ordering) || !empty($direction))
 		{
